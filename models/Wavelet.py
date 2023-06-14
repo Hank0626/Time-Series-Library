7a8d3710717b08acbfcd34d87fd8148fcb1dd1fc
lpy <1@1.com>
2023-06-14 18:29:45 +0800

    update

--- models/Wavelet.py
+++ models/Wavelet.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import torch.fft
 from layers.Embed import DataEmbedding
 from layers.Conv_Blocks import Inception_Block_V1
-import pywt
-# import ptwt
+# import pywt
+import ptwt
 import numpy as np
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
@@ -85,54 +85,6 @@ class Transformer(nn.Module):
             x = ff(x) + x
         return x
 
-# class ViT(nn.Module):
-#     def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
-#         super().__init__()
-#         image_height, image_width = pair(image_size)
-#         patch_height, patch_width = pair(patch_size)
-
-#         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
-
-#         num_patches = (image_height // patch_height) * (image_width // patch_width)
-#         patch_dim = channels * patch_height * patch_width
-#         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
-
-#         self.to_patch_embedding = nn.Sequential(
-#             Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
-#             nn.LayerNorm(patch_dim),
-#             nn.Linear(patch_dim, dim),
-#             nn.LayerNorm(dim),
-#         )
-
-#         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-#         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-#         self.dropout = nn.Dropout(emb_dropout)
-
-#         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-#         self.pool = pool
-#         self.to_latent = nn.Identity()
-
-#         self.mlp_head = nn.Sequential(
-#             nn.LayerNorm(dim),
-#             nn.Linear(dim, num_classes)
-#         )
-
-#     def forward(self, img):
-#         x = self.to_patch_embedding(img)
-#         b, n, _ = x.shape
-#         import pdb; pdb.set_trace()
-#         cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
-#         x = torch.cat((cls_tokens, x), dim=1)
-#         x += self.pos_embedding[:, :(n + 1)]
-#         x = self.dropout(x)
-
-#         x = self.transformer(x)
-
-#         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
-
-#         x = self.to_latent(x)
-#         return self.mlp_head(x)
 class ViT(nn.Module):
     def __init__(self, *, image_size, patch_size, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
         super().__init__()
@@ -175,7 +127,7 @@ class ViT(nn.Module):
         x = self.dropout(x)
 
         x = self.transformer(x)
-        x = x[:, :self.num_patches , :]
+        # x = x[:, :self.num_patches , :]
         x = self.to_latent(x)
         return self.mlp_head(x)
 
@@ -191,10 +143,10 @@ def FFT_for_Period(x, k=2):
     return period, abs(xf).mean(-1)[:, top_list]
 
 def Wavelet_for_Period(x, scale=16):
-    scales = np.arange(1, 1+scale)
-
-    coeffs, freqs = pywt.cwt(x.detach().cpu().numpy(), scales, 'morl')
-    # coeffs, freqs = ptwt.cwt(x, scales, 'morl') 
+    # scales = np.arange(1, 1+scale)
+    scales = 2 ** np.arange(-1, 7, step=0.5)
+    # coeffs, freqs = pywt.cwt(x.detach().cpu().numpy(), scales, 'morl')
+    coeffs, freqs = ptwt.cwt(x, scales, 'morl') 
     return coeffs, freqs
     
 class Wavelet(nn.Module):
@@ -232,9 +184,10 @@ class Wavelet(nn.Module):
 
         res = self.ViT(coeffs).permute(0, 3, 1, 2)
 
-        freqs =  F.softmax(torch.tensor(freqs).to(x.device)).unsqueeze(0).unsqueeze(0).unsqueeze(0).repeat(B, T, N, 1)
-        res = torch.sum(res * freqs, -1).float()
+        # freqs = torch.tensor(freqs).to(x.device).unsqueeze(0).unsqueeze(0).unsqueeze(0).repeat(B, T, N, 1)
+        # res = torch.sum(res * freqs, -1).float()
         # # residual connection
+        res = torch.sum(res, axis=-1)
         res = res + x
         return res
 
